860cdb6b55b280424011c8804fe9d2edbc4a9e7b
Bruno G. Tavares <devbruno78@gmail.com>
2017-12-27 01:46:22 -0500

    add movies.csv visualization

---
+++ view_list.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import os
+import webbrowser
+
+# Read the dataset into a data table using Pandas
+data_table = pd.read_csv('data/movies.csv', index_col="movie_id" )
+
+# Create a web page view of the data for easy viewing
+html = data_table.to_html()
+
+# Save the html to a temporary file
+with open("movie_list.html", "w") as f:
+    f.write(html)
+
+# Open the webpage in the webbrowser
+full_filename = os.path.abspath("movie_list.html")
+webbrowser.open("file://{}".format(full_filename))
